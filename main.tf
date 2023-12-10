d32e1e7ff2d94db0ae5cee30c1e3e4836566a087
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-12-10 12:15:29 +0530

    setting mongodb cluster

--- main.tf
+++ main.tf
@@ -3,3 +3,12 @@ resource "aws_docdb_subnet_group" "main" {
   subnet_ids = var.subnet_ids
   tags = merge(local.tags, {Name = "${local.name_prefix}-subnet-group" })
 }
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "${local.name_prefix}-docdb-cluster"
+  engine                  = "docdb"
+  master_username         = "foo"
+  master_password         = "mustbeeightchars"
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
+}
