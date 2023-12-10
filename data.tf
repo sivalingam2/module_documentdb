ce49c522634a2d4c02d22dc61f910b14cd962f9b
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-12-10 21:11:42 +0530

    setting mongodb cluster

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
 data "aws_ssm_parameter" "master_username" {
-  name = "docudb.${var.env}-master_username"
+  name = "docudb.${var.env}.master_username"
 }
 data "aws_ssm_parameter" "master_password" {
-  name = "docudb.${var.env}-master_password"
+  name = "docudb.${var.env}.master_password"
 }
